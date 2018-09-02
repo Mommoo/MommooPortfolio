3ce4d72e2c88c34f86300f44338a120921cdea8b
mommoo <dv.mommoo@gmail.com>
2018-09-02 16:57:30 +0900

    ## Clear Code  - remove unnecessary code
    ## Build Header View Template
     - clone element of fixed element were appended parent node because of using block property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {AnimationSetting} from './common/animationSetting';
-import {ViewportDimension, WindowEventService} from './common/window-event.service';
+import {WindowEventService} from './common/window-event.service';
 
 /**
  * naviationHeaderArea는 fixedHeader는 position fix 성질을 가진다.
@@ -14,112 +14,28 @@ import {ViewportDimension, WindowEventService} from './common/window-event.servi
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements AfterViewInit {
   @ViewChild('navigationHeaderArea')
   private navigationHeaderAreaElementRef: ElementRef;
 
   @ViewChild('fixedHeaderWrapper')
   private fixedHeaderWrapper: ElementRef;
 
-  public constructor(private windowEventService: WindowEventService) {
-  }
-
-  ngOnInit(): void {
-  }
-
   ngAfterViewInit(): void {
-    this.windowEventService.addViewportDimensionDetectListener(viewportDimension => {
-      switch (viewportDimension) {
-        case ViewportDimension.MOBILE :
-        case ViewportDimension.TABLET :
-          this.windowEventService.addViewportResizeListener('trackingHeader', ()=> {
-            this.navigationHeaderAreaElementRef.nativeElement.style.height = `${this.fixedHeaderWrapper.nativeElement.offsetHeight}px`;
-          }, true);
-          break;
-
-        case ViewportDimension.DESKTOP :
-          this.windowEventService.removeEvent('trackingHeader');
-          break;
-      }
-    }, true);
+    /** fixedHeader가 부모 영역을 벗어나기 때문에,
+     * Header를 복사한 후 fixed 성질을 지우고 부모창에 붙여 block 구조를 살린다. */
+    const fakeHeader = this.cloneHeader();
+    this.navigationHeaderAreaElementRef.nativeElement.appendChild(fakeHeader);
   }
 
-  // public onMenuClick(menuItem: MenuListItem): void {
-  //   //this.headerComponent.getHeaderHeight()
-  //   const offsetY : number = !WindowEventService.isDesktopViewport() ? 0: 0;
-  //   switch (menuItem) {
-  //     case MenuListItem.PROFILE :
-  //       AppComponent.scrollToElement(this.profileElementRef, offsetY);
-  //       break;
-  //
-  //     case MenuListItem.ABOUT :
-  //       AppComponent.scrollToElement(this.aboutElementRef, offsetY);
-  //       break;
-  //
-  //     case MenuListItem.PORTFOLIO :
-  //       AppComponent.scrollToElement(this.portfolioElementRef, offsetY);
-  //       break;
-  //
-  //     case MenuListItem.BLOG :
-  //       AppComponent.openNewWindow('https://mommoo.tistory.com');
-  //       break;
-  //
-  //     case MenuListItem.GITHUB :
-  //       AppComponent.openNewWindow('https://github.com/mommoo');
-  //       break;
-  //   }
-  // }
-
-  //TODO 애니메이션 객체 만들기..!
-  //TODO 스크롤 가는거 다시 만들기 ㅠㅠ...
-  private static scrollToElement(elementRef: ElementRef, offset: number) {
-    const startY: number = window.pageYOffset;
-    const destY: number = elementRef.nativeElement.offsetTop - offset;
-    const duration: number = AnimationSetting.DURATION;
-    let spentTime: number = 0;
-    let previousTime: number = 0;
-
-    const animationFunction = time => {
-      if (previousTime != 0) {
-        spentTime += time - previousTime;
-      }
-      previousTime = time;
-
-      let percent = Math.min(spentTime / duration, 1);
-      percent = AppComponent.easeInExpo(percent);
-
-      const currentMove = (destY - startY) * percent;
-
-      window.scroll(0, startY + currentMove);
-
-      if (spentTime < duration) {
-        window.requestAnimationFrame(animationFunction);
-      }
-    };
-
-    window.requestAnimationFrame(animationFunction);
-  }
-
-  private static openNewWindow(URL: string): void {
-    window.open(URL, '_blank');
-  }
-
-  private static easeInOutQuint(percent: number): number {
-    return percent < 0.5 ? 16 * percent * percent * percent * percent * percent : 1 + 16 * (--percent) * percent * percent * percent * percent;
-  }
-
-  private static easeInExpo(percent: number): number {
-    return (Math.pow(2, 8 * percent) - 1) / 255;
-  }
-
-  public image = '/assets/images/test.jpg';
-
-  public test() {
-    if (this.image === '/assets/images/test.jpg') {
-      this.image = '/assets/images/javascript.jpg';
-    } else {
-      this.image = '/assets/images/test.jpg';
-    }
+  private cloneHeader() : HTMLElement {
+    const cloneHeader = this.fixedHeaderWrapper.nativeElement.cloneNode(true);
+    Object.assign(cloneHeader.style, {
+      position : 'static',
+      zIndex : '1',
+      boxShadow : 'none'
+    });
+    return cloneHeader;
   }
 }
 
