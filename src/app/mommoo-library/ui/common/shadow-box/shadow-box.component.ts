4bbdfbd33fe6ca0b308ad8c81c7b89fbb92b2eeb
mommoo <dv.mommoo@gmail.com>
2018-10-17 17:11:02 +0900

    ## Remove Unnecessary @Input 'radius'

--- src/app/mommoo-library/ui/common/shadow-box/shadow-box.component.ts
+++ src/app/mommoo-library/ui/common/shadow-box/shadow-box.component.ts
@@ -8,23 +8,15 @@ import {ChangeDetectionStrategy, Component, Host, HostBinding, Input, OnChanges,
 })
 export class MommooShadowBox implements OnChanges{
   @Input()
-  public radius : string = '0px';
-
-  @Input()
   public flat: boolean = false;
-
-  @HostBinding('style.borderRadius')
-  public borderRadius: string;
 
   public targetClassName = 'shadow-visible-rect';
 
   ngOnChanges(changes: SimpleChanges): void {
     if ( this.flat ) {
       this.targetClassName = 'flat-visible-rect';
-      this.borderRadius = '0px';
     } else {
       this.targetClassName ='shadow-visible-rect';
-      this.borderRadius = this.radius;
     }
   }
 }
