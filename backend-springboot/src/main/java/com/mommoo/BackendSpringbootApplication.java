7da427db300b76cb160c40b38cc104dc81cfa041
mommoo <dv.mommoo@gmail.com>
2018-11-23 17:40:28 +0900

    build mommoo-portfolio develop environment

---
+++ backend-springboot/src/main/java/com/mommoo/BackendSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.mommoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringbootApplication.class, args);
+    }
+}
