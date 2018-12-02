1c6b13c48337961f9e9fd20f8f564d45c60a7663
mommoo <dv.mommoo@gmail.com>
2018-12-02 18:15:24 +0900

    ## Refactoring ContextProvider

--- backend-springboot/src/main/java/com/mommoo/portfolio/common/ContextProvider.java
+++ backend-springboot/src/main/java/com/mommoo/portfolio/common/ContextProvider.java
@@ -5,27 +5,39 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 /**
- * It is provide of the 'mommoo-portfolio' project's context data
+ * It is data class that the 'mommoo-portfolio' project's context data
+ *
  * Context data is to static data that decide by developer for 'mommoo-portfolio' project's properties
- * If illogical static data exist, it have to declare to this class and provide if you want
+ *
+ * If illogical static data exist,
+ * it have to declare to this application.properties file {@see classpath::resouces.application.properties}
+ * and this class provide it if you want
+ *
+ * This data will be provided to front-end, by controller {@link com.mommoo.portfolio.controller.RestDataController}
  *
  * @author mommoo
  */
 @Component
 public class ContextProvider {
-    private static final String IMAGE_REPOSITORY_PATH = "/assets";
-    private final String CONTEXT_PATH;
+    private static final String CONTEXT_PATH_PROPERTY_KEY = "server.servlet.context-path";
+    private static final String IMAGE_REPOSITORY_PROPERTY_KEY = "image.repository.path";
+    private static final String THEME_COLOR_PROPERTY_KEY = "theme.color";
+    private Environment environment;
 
     @Autowired
     public ContextProvider(Environment environment) {
-        this.CONTEXT_PATH = environment.getProperty("server.servlet.context-path");
+        this.environment = environment;
     }
 
     public String getContextPath() {
-        return CONTEXT_PATH;
+        return this.environment.getProperty(CONTEXT_PATH_PROPERTY_KEY);
     }
 
     public String getImageRepositoryPath() {
-        return IMAGE_REPOSITORY_PATH;
+        return this.environment.getProperty(IMAGE_REPOSITORY_PROPERTY_KEY);
+    }
+
+    public String getThemeColor() {
+        return this.environment.getProperty(THEME_COLOR_PROPERTY_KEY);
     }
 }
