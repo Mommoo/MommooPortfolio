ff6c55da9fb7ba04d7dbc5f1ec24786b42cc813f
mommoo <dv.mommoo@gmail.com>
2018-12-04 04:14:54 +0900

    ## Develop Domain's Resource Package

---
+++ backend-springboot/src/main/java/com/mommoo/portfolio/domain/resource/Resource.java
@@ -0,0 +1,32 @@
+package com.mommoo.portfolio.domain.resource;
+
+import lombok.ToString;
+
+import java.util.HashMap;
+
+/**
+ * This class is persistence data class that provide static-resource file path
+ *
+ * {@link #repositoryPath} is root path of static-resource
+ * {@link #icon} is icon file name finder (ex. if input "youtube", return to "youtube.svg")
+ *
+ * @author mommoo
+ */
+@ToString
+public class Resource {
+    private String repositoryPath;
+    private HashMap<String, String> icon;
+
+    public String getIconFilePathAppliedRepositoryPath(String iconName) {
+        String iconFileName = icon.get(iconName);
+        if ( iconFileName == null ) {
+            return "";
+        }
+
+        return getFilePathAppliedRepositoryPath(iconFileName);
+    }
+
+    public String getFilePathAppliedRepositoryPath(String resourceFileName) {
+        return repositoryPath + "/" + resourceFileName;
+    }
+}
