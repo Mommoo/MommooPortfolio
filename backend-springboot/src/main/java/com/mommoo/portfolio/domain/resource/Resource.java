2d31601c1c5b8735a255d837c60af293184e9733
mommoo <dv.mommoo@gmail.com>
2018-12-05 03:48:50 +0900

    ## Resource Refactoring
    ## Resource Append Rule
     - finding icon file path is use only lower case icon name

--- backend-springboot/src/main/java/com/mommoo/portfolio/domain/resource/Resource.java
+++ backend-springboot/src/main/java/com/mommoo/portfolio/domain/resource/Resource.java
@@ -8,25 +8,30 @@ import java.util.HashMap;
  * This class is persistence data class that provide static-resource file path
  *
  * {@link #repositoryPath} is root path of static-resource
- * {@link #icon} is icon file name finder (ex. if input "youtube", return to "youtube.svg")
+ * {@link #icon} is icon file name finder
+ *
+ * icon searching is used only lower case {@link #getIconFilePath(String)}
  *
  * @author mommoo
  */
+//TODO 도메인 이미지 영역과 아이콘이 배치되어 있는 영역을 구분할 것.
 @ToString
 public class Resource {
     private String repositoryPath;
     private HashMap<String, String> icon;
 
+    /** find file path using by `lower case` icon name  */
     public String getIconFilePath(String iconName) {
-        String iconFileName = icon.get(iconName);
+        String lowerCaseIconName = iconName.toLowerCase();
+        String iconFileName = icon.get(lowerCaseIconName);
         if ( iconFileName == null ) {
             return "";
         }
 
-        return getFilePath(iconFileName);
+        return getFilePathContainRepositoryPath(iconFileName);
     }
 
-    public String getFilePath(String resourceFileName) {
+    public String getFilePathContainRepositoryPath(String resourceFileName) {
         return repositoryPath + "/" + resourceFileName;
     }
 }
