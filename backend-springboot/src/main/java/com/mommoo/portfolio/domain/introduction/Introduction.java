0f9c05a62d0c8a13fef666b9960615ed78568242
mommoo <dv.mommoo@gmail.com>
2018-12-10 15:53:59 +0900

    ## Append Persistence Class Of Introduction

---
+++ backend-springboot/src/main/java/com/mommoo/portfolio/domain/introduction/Introduction.java
@@ -0,0 +1,38 @@
+package com.mommoo.portfolio.domain.introduction;
+
+import lombok.Getter;
+import lombok.ToString;
+import lombok.Value;
+
+import java.util.List;
+
+/**
+ * This is persistence class of MongoDB's collection 'introduction'.
+ * The Profile{@link #profile}, {@link Profile} is the information of developer Mommoo.
+ * The Language Technology{@link #languageTechs}, {@link LanguageTech} is the programming languages
+ * that Developer Mommoo able to using at programming develop
+ *
+ * @author mommoo
+ */
+@Getter
+@ToString
+public class Introduction {
+    private Profile profile;
+    private List<LanguageTech> languageTechs;
+
+    @Value
+    @ToString
+    public static class Profile {
+        private final String name;
+        private final int birthYear;
+        private final List<String> degree;
+    }
+
+    @Value
+    @ToString
+    public static class LanguageTech {
+        private final String name;
+        private final String image;
+        private final List<String> briefings;
+    }
+}
