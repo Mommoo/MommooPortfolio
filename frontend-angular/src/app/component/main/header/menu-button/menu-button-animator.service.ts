5ff2ea4a6416c573449eeab2efad904764b7e15c
mommoo <dv.mommoo@gmail.com>
2019-01-08 19:19:10 +0900

    ## Refactoring MenuButton Package In Header Package

--- frontend-angular/src/app/component/main/header/menu-button/menu-button-animator.service.ts
+++ frontend-angular/src/app/component/main/header/menu-button/menu-button-animator.service.ts
@@ -1,43 +1,75 @@
 import {KeyframeAnimator} from '../../../../../mommoo-library/handler/animation/keyframe/keyframe-animator';
-import {ElementRef, Injectable} from '@angular/core';
+import {ElementRef} from '@angular/core';
 import {
   AnimationKeyframe,
   KeyframeAnimationConfig,
   KeyframeAnimationType
 } from '../../../../../mommoo-library/handler/animation/keyframe/types';
 import {DomUtils} from '../../../../../mommoo-library/util/dom';
+import {AnimationKey, MenuButtonLineOffsetsTop} from '../header.types';
 
-@Injectable()
+/**
+ * This class provides that hamburger menu animation
+ * and animation callback event used by KeyframeAnimator. {@link KeyframeAnimator}
+ *
+ * The KeyframeAnimator is created by {@link LineKeyframes} class.
+ */
 export class MenuButtonAnimator {
-  private keyframeAnimator = MenuButtonAnimator.createMenuButtonKeyframeAnimator();
+  private readonly keyframeAnimator;
   private isPending;
 
-  private static createMenuButtonKeyframeAnimator() {
+  public constructor(lineHeight: number, lineGutter: number) {
+    const menuButtonLineOffsetsTop
+      = MenuButtonAnimator.computeMenuButtonLineOffsetTop(lineHeight, lineGutter);
+    this.keyframeAnimator
+      = MenuButtonAnimator.createMenuButtonKeyframeAnimator(menuButtonLineOffsetsTop);
+  }
+
+  private static computeMenuButtonLineOffsetTop(lineHeight: number, lineGutter: number)
+    : MenuButtonLineOffsetsTop {
+    return {
+      upperLineTop: 0,
+      middleLineTop: lineHeight + lineGutter,
+      lowerLineTop: (lineHeight + lineGutter) * 2
+    };
+  }
+
+  private static createMenuButtonKeyframeAnimator(menuButtonLineOffsetsTop: MenuButtonLineOffsetsTop) {
     const keyframeAnimator = new KeyframeAnimator();
-    const [normalAnimationKeyframes, reverseAnimationKeyframes] = LineKeyframes.computeKeyframes();
+    const [normalAnimationKeyframes, reverseAnimationKeyframes]
+      = LineKeyframes.computeKeyframes(menuButtonLineOffsetsTop);
+
     keyframeAnimator.addKeyframes(normalAnimationKeyframes);
     keyframeAnimator.addKeyframes(reverseAnimationKeyframes);
     return keyframeAnimator;
   }
 
-  private registerDoneListener(animationKeys: AnimationKey[], doneListener: ()=>void) {
-    const removeAnimationListenerAll = ()=> {
-      animationKeys.forEach(key=> {
-        this.keyframeAnimator.unSubscribeAnimationListener(key.animationName, key.elementRef.nativeElement);
-      });
-    };
+  private static combineToAnimationKeys(
+    elementRefs: ElementRef<HTMLElement>[], animationName: string[]): AnimationKey[] {
 
-    const animationEndChecker = new Array(animationKeys.length).fill(false, 0, animationKeys.length);
+    return elementRefs.map((elementRef, index) => ({
+      animationName: animationName[index],
+      elementRef: elementRef
+    }));
+  }
 
-    const animationCheckListener = (type, index)=>{
-      if ( type === KeyframeAnimationType.ANIMATION_END ) {
+  private registerDoneListener(animationKeys: AnimationKey[], doneListener: () => void) {
+    const animationEndChecker
+      = new Array(animationKeys.length).fill(false, 0, animationKeys.length);
+
+    const animationCheckListener = (type, index) => {
+      if (type === KeyframeAnimationType.ANIMATION_END) {
         animationEndChecker[index] = true;
       }
 
-      const animationAllEnd = animationEndChecker.every(bool=> bool);
+      const animationAllEnd = animationEndChecker.every(bool => bool);
       if (animationAllEnd) {
         doneListener();
-        removeAnimationListenerAll();
+        animationKeys
+          .forEach(key => {
+            this.keyframeAnimator
+              .unSubscribeAnimationListener(key.animationName, key.elementRef.nativeElement);
+          });
         this.isPending = false;
       }
     };
@@ -47,8 +79,8 @@ export class MenuButtonAnimator {
      * so, to prevent that problem when animation event end,
      * element change to the style that applied by animation event
      */
-    const preventIEReflowReAnimate = (key, type)=>{
-      if ( type === KeyframeAnimationType.ANIMATION_END ) {
+    const preventIEReflowReAnimate = (key, type) => {
+      if (type === KeyframeAnimationType.ANIMATION_END) {
         DomUtils.applyStyle(key.elementRef, {
           ...this.keyframeAnimator.getKeyframe(key.animationName).keyframe.to,
           animationName: ''
@@ -56,16 +88,19 @@ export class MenuButtonAnimator {
       }
     };
 
-    animationKeys.forEach((key, index)=> {
-      this.keyframeAnimator.subscribeAnimationListener(key.animationName, key.elementRef.nativeElement, type=> {
-        animationCheckListener(type, index);
-        preventIEReflowReAnimate(key, type);
-      });
+    animationKeys.forEach((key, index) => {
+      this.keyframeAnimator
+        .subscribeAnimationListener(key.animationName, key.elementRef.nativeElement, type => {
+          animationCheckListener(type, index);
+          preventIEReflowReAnimate(key, type);
+        });
     });
   }
 
-  public startAnimation(type: 'normal' | 'reverse', elementRefs: ElementRef<HTMLElement>[], doneListener: ()=>void){
-    if ( type === 'normal' ) {
+  public startAnimation(type: 'normal' | 'reverse',
+                        elementRefs: ElementRef<HTMLElement>[],
+                        doneListener: () => void) {
+    if (type === 'normal') {
       this.startAnimations(elementRefs, LineKeyframes.getNormalAnimationNames(), doneListener);
     } else {
       this.startAnimations(elementRefs, LineKeyframes.getReverseAnimationNames(), doneListener);
@@ -76,49 +111,85 @@ export class MenuButtonAnimator {
     return this.isPending;
   }
 
-  private static createAnimationKeys(elementRefs: ElementRef<HTMLElement>[], animationName:string[]): AnimationKey[] {
-    return elementRefs.map((elementRef, index)=>({
-      animationName: animationName[index],
-      elementRef: elementRef
-    }))
-  }
-
-  private startAnimations(elementRefs: ElementRef<HTMLElement>[], animationName:string[], doneListener: ()=>void) {
-    const animationKeys = MenuButtonAnimator.createAnimationKeys(elementRefs, animationName);
+  private startAnimations(elementRefs: ElementRef<HTMLElement>[],
+                          animationName: string[],
+                          doneListener: () => void) {
+    const animationKeys = MenuButtonAnimator.combineToAnimationKeys(elementRefs, animationName);
     this.registerDoneListener(animationKeys, doneListener);
     this.isPending = true;
-    animationKeys.forEach(key=> this.keyframeAnimator.startAnimation(key.animationName, key.elementRef));
+    animationKeys
+      .forEach(key => this.keyframeAnimator.startAnimation(key.animationName, key.elementRef));
   }
 }
 
+/**
+ * This class decide that each menu line element's animations shape.
+ *
+ * The animation shape is normal animation or reverse animation.
+ *
+ * Following element animation shape example is normal animation,
+ * the reverse animation is reverse to normal animation.
+ *
+ * The TopElement animation shape is rotating and moving to middle position animation.
+ * The MiddleElement animation shape is simply fade animation.
+ * The BottomElement animation shape is rotating and moving to middle position animation.
+ */
 class LineKeyframes {
-  private static readonly topLineNormalAnimationName = 'mommoo-menu-button-top-line-normal-animator';
-  private static readonly middleLineNormalAnimationName = 'mommoo-menu-button-middle-line-normal-animator';
-  private static readonly bottomLineNormalAnimationName = 'mommoo-menu-button-bottom-line-normal-animator';
+  private static manufactureAnimationNames(menuButtonAnimationType: 'normal' | 'reverse') {
+    return ['top', 'middle', 'bottom']
+      .map(location => `mommoo-menu-button-${location}-line-${menuButtonAnimationType}-animation`);
+  }
 
-  private static readonly topLineReverseAnimationName = 'mommoo-menu-button-top-line-reverse-animator';
-  private static readonly middleLineReverseAnimationName = 'mommoo-menu-button-middle-line-reverse-animator';
-  private static readonly bottomLineReverseAnimationName = 'mommoo-menu-button-bottom-line-reverse-animator';
+  private static computeNormalKeyframeValues(menuButtonLineOffsetsTop: MenuButtonLineOffsetsTop) {
+    const normalTopKeyframeValue = [
+      {
+        transform: 'rotate(0deg)',
+        top: `${menuButtonLineOffsetsTop.upperLineTop}px`
+      },
+      {
+        transform: 'rotate(-405deg)',
+        top: `${menuButtonLineOffsetsTop.middleLineTop}px`
+      }
+    ];
+
+    const normalMiddleKeyframeValue = [
+      {
+        opacity: '1'
+      },
+      {
+        opacity: '0'
+      }
+    ];
+
+    const normalBottomKeyframeValue = [
+      {
+        transform: 'rotate(0deg)',
+        top: `${menuButtonLineOffsetsTop.lowerLineTop}px`
+      },
+      {
+        transform: 'rotate(405deg)',
+        top: `${menuButtonLineOffsetsTop.middleLineTop}px`
+      }
+    ];
+    return [
+      normalTopKeyframeValue,
+      normalMiddleKeyframeValue,
+      normalBottomKeyframeValue
+    ];
+  }
 
   public static getNormalAnimationNames() {
-    return [
-      LineKeyframes.topLineNormalAnimationName,
-      LineKeyframes.middleLineNormalAnimationName,
-      LineKeyframes.bottomLineNormalAnimationName
-    ];
+    return this.manufactureAnimationNames('normal');
   }
 
   public static getReverseAnimationNames() {
-    return [
-      LineKeyframes.topLineReverseAnimationName,
-      LineKeyframes.middleLineReverseAnimationName,
-      LineKeyframes.bottomLineReverseAnimationName
-    ];
+    return this.manufactureAnimationNames('reverse');
   }
 
-  public static computeKeyframes(): [AnimationKeyframe[], AnimationKeyframe[]] {
-    const normalKeyframeValues = this.computeNormalKeyframeValues();
-    const normalAnimationConfig: KeyframeAnimationConfig= {
+  public static computeKeyframes(menuButtonLineOffsetsTop: MenuButtonLineOffsetsTop)
+    : [AnimationKeyframe[], AnimationKeyframe[]] {
+    const normalKeyframeValues = this.computeNormalKeyframeValues(menuButtonLineOffsetsTop);
+    const normalAnimationConfig: KeyframeAnimationConfig = {
       timingFunction: 'ease-in',
       fillMode: 'forwards'
     };
@@ -151,47 +222,4 @@ class LineKeyframes {
 
     return [normalAnimationKeyframes, reverseAnimationKeyframes];
   }
-
-  private static computeNormalKeyframeValues() {
-    const normalTopKeyframeValue = [
-      {
-        transform: 'rotate(0deg)',
-        top: '0'
-      },
-      {
-        transform: 'rotate(-405deg)',
-        top: '10px'
-      }
-    ];
-
-    const normalMiddleKeyframeValue = [
-      {
-        opacity: '1'
-      },
-      {
-        opacity: '0'
-      }
-    ];
-
-    const normalBottomKeyframeValue = [
-      {
-        transform: 'rotate(0deg)',
-        top: '20px'
-      },
-      {
-        transform: 'rotate(405deg)',
-        top: '10px'
-      }
-    ];
-    return [
-      normalTopKeyframeValue,
-      normalMiddleKeyframeValue,
-      normalBottomKeyframeValue
-    ];
-  }
-}
-
-interface AnimationKey {
-  animationName: string,
-  elementRef: ElementRef<HTMLElement>
 }
--- frontend-angular/src/app/component/main/header/menu-button/menu-button.component.ts
+++ frontend-angular/src/app/component/main/header/menu-button/menu-button.component.ts
@@ -1,72 +1,110 @@
-import {ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostBinding,
+  HostListener, OnInit,
+  Output,
+  ViewChild
+} from '@angular/core';
 import {MenuButtonEvent, MenuButtonState} from '../header.types';
 import {MenuButtonAnimator} from './menu-button-animator.service';
+import {DomUtils} from '../../../../../mommoo-library/util/dom';
 
+/**
+ * This class decide that both hamburger shape menu's style and host element dimension
+ *
+ * The host style properties are {@link hostWidth}, {@link hostHeight}
+ * The hamburger menu's style properties are {@link lineWidth}, {@link lineHeight} and {@link lineGutter}.
+ *
+ * It is also can invoking hamburger menu's animation used by MenuButtonAnimator
+ * {@link MenuButtonAnimator}
+ */
 @Component({
-  selector: 'view-menu-button',
+  selector: 'header-menu-button',
   templateUrl: './menu-button.component.html',
   styleUrls: ['./menu-button.component.scss'],
-  changeDetection : ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MenuButtonComponent {
-  private menuButtonState : MenuButtonState = MenuButtonState.CLOSED;
+export class MenuButtonComponent implements OnInit {
+  private static readonly lineWidth = 30;
+  private static readonly lineHeight = 3;
+  private static readonly lineGutter = 5;
+
+  @HostBinding('style.width.px')
+  private readonly hostWidth = MenuButtonComponent.lineWidth;
+
+  @HostBinding('style.height.px')
+  private readonly hostHeight = (MenuButtonComponent.lineHeight * 3) + (MenuButtonComponent.lineGutter * 2);
+
+  private menuButtonState: MenuButtonState = MenuButtonState.CLOSED;
+
+  private menuButtonAnimator: MenuButtonAnimator;
 
   @ViewChild('menuTop')
-  private menuTopElementRef : ElementRef<HTMLElement>;
+  private menuTopElementRef: ElementRef<HTMLElement>;
 
   @ViewChild('menuMiddle')
-  private menuMiddleElementRef : ElementRef<HTMLElement>;
+  private menuMiddleElementRef: ElementRef<HTMLElement>;
 
   @ViewChild('menuBottom')
-  private menuBottomElementRef : ElementRef<HTMLElement>;
+  private menuBottomElementRef: ElementRef<HTMLElement>;
 
   @Output('menuButtonEvent')
-  private menuButtonEventEmitter : EventEmitter<MenuButtonEvent> = new EventEmitter<MenuButtonEvent>();
+  private menuButtonEventEmitter: EventEmitter<MenuButtonEvent> = new EventEmitter<MenuButtonEvent>();
 
-  constructor(private menuButtonAnimator: MenuButtonAnimator) {}
-
-  public getMenuButtonState(): MenuButtonState {
-    return this.menuButtonState;
-  }
-
-  public executeButtonOpen() {
-    this.executeButtonEvent(MenuButtonEvent.OPEN);
-  }
-
-  public executeButtonClose() {
-    this.executeButtonEvent(MenuButtonEvent.CLOSE);
-  }
-
-  public toggleButtonEvent() {
-    if ( this.isPenddingToggle() ) {
-      return ;
-    }
-    const shouldBeExecuteButtonEvent = this.isButtonClosed() ? MenuButtonEvent.OPEN : MenuButtonEvent.CLOSE;
-    this.executeButtonEvent(shouldBeExecuteButtonEvent);
-  }
-
-  private executeButtonEvent(buttonEvent: MenuButtonEvent) {
-    this.menuButtonEventEmitter.emit(buttonEvent);
-
-    const menuLineElementRefs = [
-      this.menuTopElementRef,
-      this.menuMiddleElementRef,
-      this.menuBottomElementRef
-    ];
-
-    const animationType = buttonEvent === MenuButtonEvent.OPEN ? 'normal' : 'reverse';
-    this.menuButtonAnimator.startAnimation(animationType, menuLineElementRefs, ()=> this.updateButtonState());
+  public constructor() {
+    this.menuButtonAnimator
+      = new MenuButtonAnimator(MenuButtonComponent.lineHeight, MenuButtonComponent.lineGutter);
   }
 
   private isButtonClosed() {
     return this.menuButtonState === MenuButtonState.CLOSED;
   }
 
-  private isPenddingToggle() {
-    return this.menuButtonAnimator.isAnimationPending();
+  @HostListener('click')
+  private toggleButtonEvent() {
+    if (this.menuButtonAnimator.isAnimationPending()) {
+      return;
+    }
+    const shouldBeExecuteButtonEvent = this.isButtonClosed() ? MenuButtonEvent.OPEN : MenuButtonEvent.CLOSE;
+    this.executeButtonEvent(shouldBeExecuteButtonEvent);
   }
 
   private updateButtonState() {
     this.menuButtonState = this.isButtonClosed() ? MenuButtonState.OPENED : MenuButtonState.CLOSED;
+  }
+
+  private get menuLineElementRefs() {
+    return [
+      this.menuTopElementRef,
+      this.menuMiddleElementRef,
+      this.menuBottomElementRef
+    ];
+  }
+
+  private setMenuLinesStyle() {
+    this.menuLineElementRefs.forEach((elementRef, index) => DomUtils.applyStyle(elementRef, {
+      width: `${MenuButtonComponent.lineWidth}px`,
+      height: `${MenuButtonComponent.lineHeight}px`,
+      top: `${(MenuButtonComponent.lineHeight + MenuButtonComponent.lineGutter) * index}px`
+    }));
+  }
+
+  public ngOnInit(): void {
+    this.setMenuLinesStyle();
+  }
+
+  public getMenuButtonState(): MenuButtonState {
+    return this.menuButtonState;
+  }
+
+  public executeButtonEvent(buttonEvent: MenuButtonEvent) {
+    this.menuButtonEventEmitter.emit(buttonEvent);
+
+    const animationType = buttonEvent === MenuButtonEvent.OPEN ? 'normal' : 'reverse';
+    this.menuButtonAnimator
+      .startAnimation(animationType, this.menuLineElementRefs, () => this.updateButtonState());
   }
 }
