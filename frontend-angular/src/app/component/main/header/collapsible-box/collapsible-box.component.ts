cb6a9d46e2bd318db4a5159be933c868c4741627
mommoo <dv.mommoo@gmail.com>
2019-01-08 19:26:39 +0900

    ## Refactoring CollapsibleBox Package In HeaderPackage  - renaming from animation-box to collapsible box

--- frontend-angular/src/app/component/main/header/collapsible-box/collapsible-box.component.ts
+++ frontend-angular/src/app/component/main/header/collapsible-box/collapsible-box.component.ts
@@ -2,13 +2,17 @@ import {ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef} from 
 import {animate, AnimationBuilder, style} from '@angular/animations';
 import {DomUtils} from '../../../../../mommoo-library/util/dom';
 
+/**
+ * This class is provides drop-down animations.
+ * ( expand to lower direction or collapse to upper direction )
+ */
 @Component({
-  selector: 'animation-box',
-  templateUrl: './animation-box.component.html',
-  styleUrls: ['./animation-box.component.scss'],
+  selector: 'collapsible-box',
+  templateUrl: './collapsible-box.component.html',
+  styleUrls: ['./collapsible-box.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AnimationBoxComponent {
+export class CollapsibleBoxComponent {
   public visibility = false;
 
   public constructor(private hostElementRef: ElementRef<HTMLElement>,
@@ -28,16 +32,16 @@ export class AnimationBoxComponent {
   public startExpand(animate: boolean) {
     this.setVisibility(true);
     if ( animate ) {
-      const fromHeight : number = 0;
-      const toHeight   : number = DomUtils.position(this.hostElementRef).height;
+      const fromHeight = 0;
+      const toHeight = DomUtils.position(this.hostElementRef).height;
       this.startHeightChangeAnimation(fromHeight, toHeight);
     }
   }
 
   public startCollapse(animate: boolean) {
     if ( animate ) {
-      const fromHeight: number = DomUtils.position(this.hostElementRef).height;
-      const toHeight: number = 0;
+      const fromHeight = DomUtils.position(this.hostElementRef).height;
+      const toHeight = 0;
       this.startHeightChangeAnimation(fromHeight, toHeight, () => this.setVisibility(false));
     } else {
       this.setVisibility(false);
@@ -49,7 +53,7 @@ export class AnimationBoxComponent {
     this.changeDetector.detectChanges();
   }
 
-  private startHeightChangeAnimation(fromHeight : number, toHeight : number, callback : ()=>void = ()=>{}) : void {
+  private startHeightChangeAnimation(fromHeight: number, toHeight: number, callback: () => void = () => {}): void {
     const animator = this.animationBuilder.build([
       style({height : fromHeight}),
       animate('300ms ease-in', style({
@@ -57,7 +61,7 @@ export class AnimationBoxComponent {
       }))
     ]).create(this.hostElementRef.nativeElement);
 
-    animator.onDone(()=>{
+    animator.onDone(() => {
       callback();
       animator.destroy();
     });
