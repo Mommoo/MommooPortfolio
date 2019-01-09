f82907b5cbaba2000b7f29b02d157fd981f0df28
mommoo <dv.mommoo@gmail.com>
2019-01-09 22:46:47 +0900

    ## Build FooterComponent

--- frontend-angular/src/app/component/main/footer/footer.component.ts
+++ frontend-angular/src/app/component/main/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {AppImageNameType, AppImagePathFinder} from '../../../app-image-finder.service';
+import {FooterContents} from './footer.types';
 
 @Component({
   selector: 'view-footer',
@@ -6,6 +8,44 @@ import {Component} from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  private _isReadyImage;
+  private footerContents: FooterContents[];
 
-  constructor() { }
+  public constructor(appImagePathFinder: AppImagePathFinder, changeDetector: ChangeDetectorRef) {
+    const subscription = appImagePathFinder.observable.subscribe(imageMap => {
+        this.footerContents = [
+          {
+            icon: imageMap.get(AppImageNameType.GITHUB),
+            text: 'https://github.com/Mommoo',
+            onClick() {
+              alert('github!');
+            }
+          },
+          {
+            icon: imageMap.get(AppImageNameType.TISTORY),
+            text: 'https://mommoo.tistory.com',
+            onClick() {
+              alert('tistory!!');
+            }
+          },
+          {
+            icon: imageMap.get(AppImageNameType.MAIL),
+            text: 'dv.Mommoo@gmail.com',
+            onClick() {
+              alert('mail!');
+            }
+          }
+        ];
+        changeDetector.detectChanges();
+        subscription.unsubscribe();
+      });
+  }
+
+  public get isReadyImage() {
+    return this._isReadyImage;
+  }
+
+  public get contents() {
+    return this.footerContents;
+  }
 }
---
+++ frontend-angular/src/app/component/main/footer/footer.types.ts
@@ -0,0 +1,5 @@
+export interface FooterContents {
+  readonly icon: string;
+  readonly text: string;
+  onClick();
+}
